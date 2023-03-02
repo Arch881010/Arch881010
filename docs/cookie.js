7117d435157d255f0732a7e16aff83bcf37c9b61
Arch <111789310+Arch881010@users.noreply.github.com>
2023-03-02 09:57:55 -0600

    Feat: Make accurate visit count.

--- docs/cookie.js
+++ docs/cookie.js
@@ -3,8 +3,4 @@ var visitCount = Cookies.get('visitCount');
 if (visitCount == undefined) var visitCount = 0;
 visitCountNumer = Number(visitCount) + 1
 Cookies.set('visitCount',visitCountNumer);
-document.getElementById('visits').innerHTML = String(visitCount);
-if (visitCount > 11) {
-    Cookies.remove('visitCount');
-    window.alert("Calm down. You do not need to visit this website 12+ times!")
-}
+document.getElementById('visits').innerHTML = String(visitCountNumer);
