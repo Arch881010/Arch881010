cff98712fa4a475e649383435bf09dc3b9f60ca3
Arch <111789310+Arch881010@users.noreply.github.com>
2023-03-02 09:37:49 -0600

    FIx: Value error.

--- docs/cookie.js
+++ docs/cookie.js
@@ -1,5 +1,5 @@
 
 var visitCount = Cookies.get('visitCount');
 if (visitCount == undefined) var visitCount = 0;
-Cookies.set('visitCount',(visitCount + 1))
+Cookies.set('visitCount',(Number(visitCount) + 1))
 document.getElementById('visits').innerHTML = String(visitCount);
